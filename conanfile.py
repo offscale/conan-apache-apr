8b31f49fd9525937df9c4a1641eb1d65dc0d396b
Javier G. Sogo <jgsogo@gmail.com>
2018-03-01 11:09:23 +0100

    linux: configure && make

--- conanfile.py
+++ conanfile.py
@@ -18,14 +18,21 @@ class ApacheaprConan(ConanFile):
         tools.unzip(self.lib_name + file_ext)
 
     def build(self):
-        if self.settings.build_type == "Debug":
-            tools.replace_in_file(os.path.join("apr-{v}".format(v=self.version), 'CMakeLists.txt'),
-                                  "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/libapr-1.pdb)",
-                                  "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/Debug/libapr-1.pdb)")
-        cmake = CMake(self)
-        cmake.configure(source_folder="apr-" + self.version)
-        cmake.build()
-        cmake.install()
+        if self.settings.os == "Windows":
+            if self.settings.build_type == "Debug":
+                tools.replace_in_file(os.path.join("apr-{v}".format(v=self.version), 'CMakeLists.txt'),
+                                      "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/libapr-1.pdb)",
+                                      "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/Debug/libapr-1.pdb)")
+            cmake = CMake(self)
+            cmake.configure(source_folder="apr-" + self.version)
+            cmake.build()
+            cmake.install()
+        else:
+            env_build = AutoToolsBuildEnvironment(self)
+            with tools.environment_append(env_build.vars):
+                self.run("./configure --prefix={}".format(self.package_folder))
+                self.run("make")
+                self.run("make install")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
