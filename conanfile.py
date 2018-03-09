1accb173fcbd66313ca1fef69074b7f4c1059031
Javier G. Sogo <jgsogo@gmail.com>
2018-03-09 12:11:24 +0100

    add conan flags

--- conanfile.py
+++ conanfile.py
@@ -8,23 +8,36 @@ class ApacheAPR(ConanFile):
     url = "https://github.com/jgsogo/conan-apache-apr"
     homepage = "https://apr.apache.org/"
     license = "http://www.apache.org/LICENSE.txt"
-    exports_sources = ["LICENSE",]
+    exports_sources = ["LICENSE", ]
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
+    generators = "cmake"
 
     lib_name = "apr-" + version
+
+    def configure(self):
+        del self.settings.compiler.libcxx  # It is a C library
 
     def source(self):
         file_ext = ".tar.gz" if not self.settings.os == "Windows" else "-win32-src.zip"
         tools.get("http://archive.apache.org/dist/apr/apr-{v}{ext}".format(v=self.version, ext=file_ext))
 
     def patch(self):
+        tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
+                              "# Generated .h files are stored in PROJECT_BINARY_DIR, not the",
+                              """
+                              message(">>>>>>>>>>>> ${PROJECT_BINARY_DIR}/apr_escape_test_char.h")
+                              include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                              conan_basic_setup()
+                              # Generated .h files are stored in PROJECT_BINARY_DIR, not the
+                              """)
+
         if self.settings.os == "Windows":
             if self.settings.build_type == "Debug":
                 tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
                                       "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/libapr-1.pdb)",
-                                      "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/Debug/libapr-1.pdb)")
+                                      "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/bin/libapr-1.pdb)")
             tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
                                   "INSTALL(FILES ${APR_PUBLIC_HEADERS_STATIC} ${APR_PUBLIC_HEADERS_GENERATED} DESTINATION include)",
                                   "INSTALL(FILES ${APR_PUBLIC_HEADERS_STATIC} ${APR_PUBLIC_HEADERS_GENERATED} DESTINATION include/apr-1)")
@@ -39,6 +52,7 @@ class ApacheAPR(ConanFile):
         self.patch()
         if self.settings.os == "Windows":
             cmake = CMake(self)
+
             cmake.configure(source_folder=self.lib_name)
             cmake.build()
             cmake.install()
